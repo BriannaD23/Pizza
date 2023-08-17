c0d5ad6731d1d1d8d6f4c4fc5626cb68ef52294f
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-08-17 13:21:14 -0700

    updated chnages

--- js/main.js
+++ js/main.js
@@ -2,23 +2,25 @@
 
 const header= document.querySelector('header');
 
+/*
 window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky" , window.scrollY > 0)
-});
+    header.classList.toggle("sticky" , window.scrollY > 0);
+});*/
 
-let menu = document.querySelector('#menu-icon')
-let navbar = document.querySelector('.navbar')
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x')
-    menu.classList.toggle('open')
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
 
 }
 
 window.onscroll = () =>{
-    menu.classList.remove('bx-x')
-    menu.classList.remove('open')
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
 
 }
 
@@ -30,5 +32,5 @@ const sr = ScrollReveal ({
 
 sr.reveal('.home-text',{delay: 200, origin:'left'})
 sr.reveal('.home-img',{delay: 200, origin:'right'})
-sr.reveal('.container, .about, .menu , .contact',{delay: 200, origin:'right'})
+sr.reveal('.container, .about, .menu , .contact',{delay: 200, origin:'bottom'})
 
